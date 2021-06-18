ec27e8571115ee5c79ccae64f59b1076b81c1500
reiniaust <55030087+reiniaust@users.noreply.github.com>
2021-06-18 14:53:40 +0200

    Ausgabe aller Parameter und Ergebnisse

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ let Parameter = {
 }
 
 let Material = {
-    PreisProKg: 2        // Preis/kg
+    PreisProKg: 2        
 }
 
 let Teil = {
-    Rohteilgewicht: 5,
+    Rohgewicht: 5,
     Materialkosten_Rohteil() {  
-        return this.Rohteilgewicht * Material.PreisProKg
+        return this.Rohgewicht * Material.PreisProKg
     },
-    Schmelzverlust_Prozent_fest: 5.6,   // Abbrand
+    Schmelzverlust_Prozent: 5.6,        // Abbrand
     Wandstaerke_max_mm: 15,
     Wandstaerke_min_mm: 8,
     Korrektur_Modellanzahl_Prozent: 2,  // F$47
     Kastennutzung_Prozent: 1.3,         // F$58
     Aufschlag_TeileDicke_Prozent() {    // F$44
-        return Math.max(0, 0.025 * (this.Wandstaerke_max_mm - 8) / 4)
+        return Math.max(0, 0.025 * (this.Wandstaerke_max_mm - 8) / 4) * 100
     },
     Aufschlag_Kastennutzung_Prozent() { // F$45
         return Math.max(0, 0.2 * (40 - this.Kastennutzung_Prozent))
@@ -26,8 +26,25 @@ let Teil = {
         return Parameter.Anteil_Angussgewicht_Prozent + this.Aufschlag_TeileDicke_Prozent() + this.Aufschlag_Kastennutzung_Prozent() + this.Korrektur_Modellanzahl_Prozent
     },
     Schmelzverlust_Kosten() {
-        return this.Materialkosten_Rohteil() * (1 + this.Anguss_Prozent() / 100) * this.Schmelzverlust_Prozent_fest / 100
+        return this.Materialkosten_Rohteil() * (1 + this.Anguss_Prozent() / 100) * this.Schmelzverlust_Prozent / 100
     }
 }
 
-alert(Teil.Aufschlag_Kastennutzung_Prozent())
+var json = JSON.stringify(Teil, function(key, value) {
+  if (typeof value === "function") {
+    return "/Function(" + value.toString() + ")/"
+  }
+  return value
+})
+
+let out = ''
+for (const [key, value] of Object.entries(Teil)) {
+    if (value.toString().includes("()")) {
+        out += `${key}: ${eval("Math.round(Teil." + key + "() * 100) / 100")}` + '<br>'
+    } else {
+        out += `${key}: ${value}` + '<br>'
+    }
+}
+// out = eval("Teil.Anguss_Prozent()")
+document.getElementById("data").innerHTML = out
+
